1caab34f2887404ead514de3005f4513199953a3
AkaBoy <akachiugwa@yahoo.com>
2017-08-22 18:10:46 +0000

    syntax fix

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -2,12 +2,13 @@ class profile::nginx(
   Array $vhosts,
 ) {
   include ::nginx
-  
+  notify { "testdata":
+    message => $vhosts;
+  }
   $vhosts.each |$vhost| {
     ::nginx::vhost { $vhost['fqdn']:
-      port => $vhost['port'],
-      server_aliases => $vhost['server-aliases'],
+      port            => $vhost['port'],
+      server_aliases  => $vhost['server_aliases']
     }
   }
-
 }
